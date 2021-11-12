f3dd3df08452e5145b6d5ec68f0bd2fe50360b28
Francesco Pasa <francescopasa@gmail.com>
2021-11-12 20:49:49 +0100

    stack

---
+++ src/data_structures/stack.py
@@ -0,0 +1,14 @@
+"""This is a ridiculous file, you can use a list instead of this class!"""
+
+class Stack:
+    def __init__(self):
+        self._stack = []
+
+    def insert(self, value):
+        self._stack.append(value)
+
+    def pop(self):
+        return self._stack.pop()
+
+    def peek(self):
+        return self._stack[-1]
